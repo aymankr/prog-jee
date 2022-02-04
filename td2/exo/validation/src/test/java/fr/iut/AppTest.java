4731662375be61d0bbc3f40518553c10ab2fbf76
Charasson Gabin <gabin.charasson@etu.u-bordeaux.fr>
2022-02-04 11:25:27 +0100

    td2 debut

--- td2/exo/validation/src/test/java/fr/iut/AppTest.java
+++ td2/exo/validation/src/test/java/fr/iut/AppTest.java
@@ -1,6 +1,5 @@
 package fr.iut;
 
-<<<<<<< HEAD
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
@@ -8,13 +7,11 @@ import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-=======
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import jakarta.validation.*;;
 import org.junit.jupiter.api.*;
 import java.util.Set;
->>>>>>> test fonctionnent pas O.O AAAAAAAAAAAAAAA
 
 import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,25 +19,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
-<<<<<<< HEAD
     private  static Validator validator;
-=======
-
-
-    private static Validator validator;
->>>>>>> test fonctionnent pas O.O AAAAAAAAAAAAAAA
     private Set<ConstraintViolation<Person>> violations;
 
     @BeforeAll
     public static void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
-<<<<<<< HEAD
-    }
-
-    @Test
-    public void testNOk() {
-=======
     }
 
 
@@ -51,7 +36,6 @@ public class AppTest {
 
     @Test
     public void testPerson() {
->>>>>>> test fonctionnent pas O.O AAAAAAAAAAAAAAA
         Person p2 = new Person();
         p2.setFirstName("21");
         p2.setLastName("a");
@@ -60,9 +44,4 @@ public class AppTest {
         violations = validator.validate(p2);
         assertEquals(4, violations.size());
     }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> test fonctionnent pas O.O AAAAAAAAAAAAAAA
 }
