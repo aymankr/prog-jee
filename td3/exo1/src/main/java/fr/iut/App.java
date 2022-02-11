4b979bf08edcf0f81276540d892aa58b0b127dbc
aykachmar <aykachmar@u-bordeaux.fr>
2022-02-11 09:12:21 +0100

    creation projet golf

---
+++ td3/exo1/src/main/java/fr/iut/App.java
@@ -0,0 +1,17 @@
+package fr.iut;
+
+/**
+ * Basic implementation
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Caddy caddy = new Caddy();
+        Player player = new Player("John", caddy);
+        System.out.println(player);
+        player.play(0.8, Math.PI / 2, Conditions.FAIRWAY);
+        System.out.println(player);
+        player.play(0.1, Math.PI / 2, Conditions.GREEN);
+        System.out.println(player);
+    }
+}
