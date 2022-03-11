e65f624df6028fff89d6d4d9f8335dc9d26cf72e
aykachmar <aykachmar@u-bordeaux.fr>
2022-03-11 09:31:36 +0100

    guice servlet on going

---
+++ td4/helloServlet/src/main/java/fr/iut/Room.java
@@ -0,0 +1,13 @@
+package fr.iut;
+
+public class Room {
+    public String name;
+    public int capacity;
+    public int occupation;
+
+    public Room(String name, int capacity, int occupation) {
+        this.name = name;
+        this.capacity = capacity;
+        this.occupation = occupation;
+    }
+}
